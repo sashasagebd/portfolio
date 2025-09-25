df1c56c7b6eb56f1005257fc696621073d99a670
Sasha <sashasagebd@gmail.com>
2025-09-25 15:12:52 -0700

    highlight navbar

--- index.js
+++ index.js
@@ -7,3 +7,30 @@ function contactClick() {
         container.style.display = "none";
     }
 }
+
+function getPage() {
+    let path = window.location.pathname
+    if(path === "/" || path === "") {
+        path = "/index.html";
+    }
+    return path.split("/").pop();
+}
+
+function highlightNav() {
+    const page = getPage();
+    const links = document.querySelectorAll(".nav-container a");
+    links.forEach(link => {
+        const linkPage = link.getAttribute("href").split("/").pop();
+
+        if(linkPage === page) {
+            link.style.backgroundColor = "AliceBlue";
+        }
+        else {
+            link.style.backgroundColor = "";
+        }
+    })
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    highlightNav();
+});
